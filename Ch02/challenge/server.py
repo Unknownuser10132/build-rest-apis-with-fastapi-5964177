1f5ac1a177bdb21e52642c904a8a688e911c419d
Resunwonknu <63773788+Unknownuser10132@users.noreply.github.com>
2025-02-24 03:33:38 +0000

    Chapter2 challenge done

--- Ch02/challenge/server.py
+++ Ch02/challenge/server.py
@@ -4,3 +4,19 @@
 - time: the current time in ISO format
 - user: The user running the application (hint: USER environment variable)
 """
+
+from fastapi import FastAPI
+from datetime import datetime, UTC
+from os import environ
+
+
+app = FastAPI()
+
+
+@app.get("/info")
+def info():
+  return {
+    "version": "0.1.0",
+    "time": datetime.now(tz=UTC).isoformat(),
+    "user": environ["USER"],
+  }
